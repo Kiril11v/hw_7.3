f5bcecaf40380b475978f5be8c38b58e8771382f
Kyrylo_Kud <kudkiril@gmail.com>
2025-06-27 22:43:48 +0200

    function with loop(changed)

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 // function with loop
 
-let userNumberInput = null;
-
-
 
 function getUserNumberOver100 () {
 
-    for(let i = 0; i <= 10; i++) {
-        userNumberInput = prompt('Enter a number greater than 100:');
+    const lastEnteredNumbers = [];
+
+    for(let i = 0; i < 10; i++) {
+       const userNumberInput = prompt('Enter a number greater than 100:');
 
     if (userNumberInput === null) {
        alert('Ok, bye');
@@ -22,9 +21,11 @@ function getUserNumberOver100 () {
     }
 
     const num = Math.abs(+userNumberInput);
+    lastEnteredNumbers.push(num);
 
-    if (num > 100){
-       alert ('your last number is ' + num);
+    if (num > 100){       
+alert(`You entered number greater than 100.
+All entered numbers: ${lastEnteredNumbers.join(", ")}`);
        return;
     }
     else {
@@ -33,28 +34,29 @@ function getUserNumberOver100 () {
    }
 
 
-   alert('You have exhausted 10 attempts.');
+alert(`You have exhausted 10 attempts.
+All entered numbers: ${lastEnteredNumbers.join(", ")}`);
 }
 
 
 
-function getUserNumberOver100ValidationError (userNumberInput) { // validation
+function getUserNumberOver100ValidationError (input) { // validation
 
-    const num = +userNumberInput;
+    const num = +input;
 
-    if (!userNumberInput.trim()) {
+    if (!input.trim()) {
         return 'Nothing entered';
-    }
-
-   if (!Number.isInteger(num)) {
-        return 'Not a whole number';
     }
 
     if (isNaN(num)) {
         return 'It is not a number';
     }
+
+   if (!Number.isInteger(num)) {
+        return 'Not a whole number';
+    }
     
-    return null // it's ok
+    return null // гуд
 }
 
 
